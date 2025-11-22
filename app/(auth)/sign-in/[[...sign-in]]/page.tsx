84299e1896e6625c75be6d7290a2ad73c6f40ee2
ajilkumar <ajilk0325@gmail.com>
2025-11-22 20:38:18 +0530

    Created sign in and sing up page using clerk

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { SignIn } from "@clerk/nextjs";
+import Link from "next/link";
+
+export default function SignInPage() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full">
+      <SignIn routing="path" path="/sign-in" />
+
+      <div className="mt-6 text-center">
+        <p className="text-sm text-slate-500">
+          Don’t have an account?{" "}
+          <Link
+            href="/sign-up"
+            className="font-medium text-indigo-600 hover:underline"
+          >
+            Create account
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+}
