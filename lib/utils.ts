a0e76681bd72dfaa96d9ec6f613276b88056d734
ajilkumar <ajilk0325@gmail.com>
2026-01-12 21:54:15 +0530

    Make currency dynamic based on user/group preference, defaulting to INR

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,10 +5,10 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function formatCurrency(amount: number | string) {
+export function formatCurrency(amount: number | string, currency: string = "INR") {
   const num = typeof amount === "string" ? parseFloat(amount) : amount;
-  return new Intl.NumberFormat("en-US", {
+  return new Intl.NumberFormat("en-IN", {
     style: "currency",
-    currency: "USD", // Should be dynamic in future, defaulting for now
+    currency: currency,
   }).format(num);
 }
